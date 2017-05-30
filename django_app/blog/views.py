501a23226ef928bdd3c6e9417f90fb829924a791
Jung YunHo <saijdu2198@gmail.com>
2017-05-29 20:33:51 -0700

    add templates, modify views.py, settings.py, urls.py

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context = {
+        'title': 'PostList from post_list view',
+        'posts' : posts,
+    }
+    return render(request, 'blog/post_list.html', context=context)
